8b023af58f6631294ae17b912967b6b7434bc49e
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-05-01 21:23:45 -0500

    this isnt gonna work

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://stevendepauw:lq0DDomJVlQL40El@bookdb.4vfw1yh.mongodb.net/bookdb?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
